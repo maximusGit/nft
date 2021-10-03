9a16819a0862e483c94dbd76e8323974f2e8dded
maximusGit <max.kocherga@yahoo.com>
2021-10-03 11:48:43 +0300

    Revert "Merge branch 'frontend'"
    This reverts commit 15f1d920a5721886659f2cd38496618b605cf38e, reversing
    changes made to dc94659e869917fcebfd56c496caff28d755cf1a.

--- js/packages/web/src/components/Footer/index.tsx
+++ js/packages/web/src/components/Footer/index.tsx
@@ -1,70 +1,11 @@
 import React from 'react';
 import { GithubOutlined, TwitterOutlined } from '@ant-design/icons';
-import { Button, Col, Row, Input, Typography } from 'antd';
-import { List } from 'react-content-loader';
+import { Button } from 'antd';
 
-
-export const FooterBar = () => {
-
-
-  const FooterSubscription = ()=>{
-    return(
-        <>
-        <Typography.Title level={2} style={{textAlign:"center", letterSpacing:"2px"}}><strong>For Artists</strong></Typography.Title>
-          <p style={{textIndent: '20px', borderBottom:'1px solid grey'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quam, exercitationem cupiditate veritatis modi natus?</p>
-          <h4 style={{textAlign:'center'}}> Subscribe to our newsletter </h4>
-          <Row gutter={[8,8]}>
-            <Col>
-              <Input placeholder="Email Adress"></Input>
-            </Col>
-            <Col>
-              <Button type="primary" style={{borderRadius:'5px'}}><strong>SUBSCRIBE</strong></Button>
-            </Col>
-          </Row>  
-          </>
-      
-    )
-  }
-  const FooterInfo = () =>{
-    return(
-          <Row gutter={[48,48]}>
-            <Col span={8}>
-              <ul type={'none'}> 
-  
-                <li><Typography.Title level={4}><strong>For Artists</strong></Typography.Title></li>
-                <li>Submit artist Profile</li>
-              </ul>
-            </Col>
-            <Col span={8}>
-            <ul type={'none'}>
-            <li><Typography.Title level={4}><strong>Community</strong></Typography.Title></li>
-                <li>Submit artist Profile</li>
-                <li>Editorial</li>
-                <li>Discord</li>
-                <li>Instagram</li>
-                <li>Twitter</li>
-                <li>Blog</li>
-                <li>Help center</li>
-              </ul>
-            </Col>
-            <Col span={8}>
-            <ul type={'none'}>
-            <li><Typography.Title level={4}><strong>Legle</strong></Typography.Title></li>
-                <li>Community</li>
-                <li>Guidelines</li>
-                <li>Terms Of Service</li>
-                <li>Privacy Policy</li>
-                <li>Report Content</li>
-                <li>Blug Bounty</li>
-                <li>Program</li>
-              </ul>
-            </Col>
-          </Row>
-    )
-  }
+export const Footer = () => {
   return (
-    <>
-      {/* <Button
+    <div className={'footer'}>
+      <Button
         shape={'circle'}
         target={'_blank'}
         href={'https://github.com/metaplex-foundation/metaplex'}
@@ -77,18 +18,6 @@ export const FooterBar = () => {
         href={'https://twitter.com/solana'}
         icon={<TwitterOutlined />}
       ></Button>
-      <Button className="btn btn-primary">freffererfref</Button> */}
-      <Row gutter={[16,16]} >
-        <Col span={8}>
-          <FooterSubscription/>
-        </Col>
-        <Col span={16}>
-          <FooterInfo/>
-        </Col>
-
-        
-
-      </Row>
-    </>
+    </div>
   );
 };
--- js/packages/web/src/views/home/index.tsx
+++ js/packages/web/src/views/home/index.tsx
@@ -5,7 +5,6 @@ import { useMeta } from '../../contexts';
 import { AuctionListView } from './auctionList';
 import { SetupView } from './setup';
 
-
 export const HomeView = () => {
   const { isLoading, store } = useMeta();
   const { isConfigured } = useStore();
